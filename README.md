6b0f1917eaaa552aacaec8264f8d3ab0e033e469
hatarakanai <hatarakanai.inc@gmail.com>
2024-08-13 03:18:14 +0900

    add chapter5

--- README.md
+++ README.md
@@ -9,12 +9,25 @@ pip install openai==0.28.1
 
 pip install python-dotenv
 
-# chapter4
+# chapter4_1
 pip install requests
 
 pip install sounddevice
 
-# chapter4_aaa
+# chapter4_2
 pip install numpy
 
 pip install soundfile
+
+# chapter5
+pip install obsws-python
+
+pip install pytchat
+
+# 課題
+- venvよくわかってない
+  普通にpipしちゃってるけど
+  package.jsonみたいなものってないの？
+
+- pythonのバージョンが、3.11.5固定
+  複数バージョンで管理できなかったっけ？
---
+++ chapter5/youtube_comment_adapter.py
@@ -0,0 +1,33 @@
+import pytchat
+import json
+
+class YoutubeCommentAdapter:
+  def __init__(self, video_id) -> None:
+    self.chat = pytchat.create(video_id=video_id, interruptable=False)
+
+  def get_comment(self):
+    # コメントを一括で取得
+    comments = self.__get_comments()
+    if(comments == None):
+      return None
+    comment = comments[-1] # 最新のコメント
+    # コメント情報の中からコメントのみを取得
+    message = comment.get("message")
+    return message
+
+  def __get_comments(self):
+    if(self.chat.is_alive()==False):
+      print("開始してません")
+      return None
+    comments = json.load(self.chat.get().json())
+    if(comments==[]):
+      print("コメントが取得できませんでした。")
+      return None
+    return comments
+
+if __name__ == "__main__":
+  import time
+  video_id = "任意のvideo_id"
+  chat = YoutubeCommentAdapter(video_id)
+  time.sleep(1)
+  print(chat.get_comment())
